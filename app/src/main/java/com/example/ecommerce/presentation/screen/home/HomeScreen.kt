55beef7730ffcb359bbd33a3af712b5a3d1090e6
soekhant01 <soekhant.dev@gmail.com>
2026-01-10 23:25:49 +0630

    TopAppBar implemented on home screen

--- app/src/main/java/com/example/ecommerce/presentation/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/ecommerce/presentation/screen/home/HomeScreen.kt
@@ -2,20 +2,85 @@ package com.example.ecommerce.presentation.screen.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ecommerce.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
+    Scaffold(
 
-        Text("Home Screen")
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.White,
+
+                    ),
+                title = {
+                    Row(
+                        modifier = Modifier.fillMaxWidth().padding(top = 12.dp, bottom = 12.dp, start = 8.dp, end = 8.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Column(modifier = modifier) {
+
+                            Text("ET-Shop", fontWeight = FontWeight.Bold)
+                            Text("5,000+ products and categories", fontSize = 12.sp, fontWeight = FontWeight.Light)
+                        }
+                        IconButton(onClick = {}) {
+                            Icon(
+                                modifier = Modifier.size(30.dp),
+                                painter = painterResource(R.drawable.ic_cart_filled),
+                                contentDescription = null
+                            )
+                        }
+                    }
+
+                },
+
+            )
+        }
+
+    ) { innerPadding ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Home")
+        }
+
     }
+}
+
+@Preview
+@Composable
+private fun HomePreview() {
+    HomeScreen()
 }
